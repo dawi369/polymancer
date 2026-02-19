42e9e08e0bce3a1627d42e52e6b4d4f5ec79e91c
dawi369 <david.erwin.cz68@gmail.com>
2026-02-19 09:43:26 +0100

    init app

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,10 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withUniwindConfig(config, {
-  cssEntryFile: './global.css',
-  dtsFile: './uniwind-types.d.ts',
-  extraThemes: ['dark'],
+  cssEntryFile: "./global.css",
+  dtsFile: "./uniwind-types.d.ts",
+  extraThemes: ["dark"],
+  // debug: true,
 });
